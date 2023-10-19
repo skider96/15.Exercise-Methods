f6e5fa7b9deaba4bfbbfe42aa126c3255e496765
Д. М <skider96@abv.bg>
2023-10-19 19:48:36 +0300

    fixings

--- 03.CharactersInRange/Program.cs
+++ 03.CharactersInRange/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args, string? s)
         {
-            Console.WriteLine("Hello, World!");
+            List<char> symbols = new List<char>(2);
+
+            List<char> symbolsFromAscii = new List<char>();
+
+            for (var i = 0; i <= 2; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                symbols.Add(input);
+            }
+            if (symbols[0] > symbols[1])
+            {
+                symbols.Reverse();
+            }
+
+            for (var i = symbols[0] ;i < symbols[1]; i++)
+            {
+                symbolsFromAscii.Add(symbols[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", symbolsFromAscii));
+
         }
     }
 }
