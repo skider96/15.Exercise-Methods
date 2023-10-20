cf2d7d68e3600ca262d8611d7d510c571ae7e2fa
Д. М <skider96@abv.bg>
2023-10-20 23:37:04 +0300

    problem solved

--- 06.MiddleCharacters/Program.cs
+++ 06.MiddleCharacters/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //You will receive a single string.Create a method that prints the character found at its middle. If the length of the
+            //string is even, there are two middle characters.
+
+            string input = Console.ReadLine();
+            var firstChar = FirstChar(input);
+
+            char secondChar = '\0';
+            if (input.Length % 2 == 0) 
+            {
+                secondChar = SecondChar(input);
+                Console.WriteLine($"{firstChar}{secondChar}");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(firstChar);
+            }
+        }
+
+        static char SecondChar(string input)
+        {
+            char secondChar;
+            secondChar = input[input.Length / 2];
+            return secondChar;
+        }
+
+        private static char FirstChar(string? input)
+        {
+            char firstChar = '\0';
+            for (var i = 1; i <= (input.Length - 1) / 2; i++)
+            {
+                firstChar = input[i];
+            }
+
+            return firstChar;
         }
     }
 }
