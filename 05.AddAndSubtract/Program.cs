bbd072f9c09c78ca9d7e3951f1bf3ffb8742489b
Д. М <skider96@abv.bg>
2023-10-20 22:29:54 +0300

    problem solved

--- 05.AddAndSubtract/Program.cs
+++ 05.AddAndSubtract/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Substraction(numbers);
+
+            Console.WriteLine(Substraction(numbers));
+        }
+
+        private static int Substraction(int[] numbers)
+        {
+            int final = SumOfFirstNumbers(numbers) - numbers[2];
+            return final;
+        }
+
+        static int SumOfFirstNumbers(int[] numbers)
+        {
+             int sum = numbers[0] + numbers[1];
+             return sum;
         }
     }
 }
