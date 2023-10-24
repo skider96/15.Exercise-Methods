7007a814f1d2e091c4f23aca15d2619851e3a57f
skider96 <danailqkiq@gmail.com>
2023-10-24 19:42:08 +0300

    problems solved

--- 08.FactorialDivision/Program.cs
+++ 08.FactorialDivision/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            double factorialFirstNumber = 1;
+            factorialFirstNumber = FactorialDivision(firstNumber, factorialFirstNumber);
+            double factorialSecondNumber = 1;
+            factorialSecondNumber = FactorialDivision(secondNumber, factorialSecondNumber);
+
+            double sumOfTheNumbers = factorialFirstNumber / factorialSecondNumber;
+            System.Console.WriteLine($"{sumOfTheNumbers:f2}");
+
+
+        }
+
+        static double FactorialDivision(int Number, double factorialNumber)
+        {
+            if (Number == 0)
+            {
+                Number = 1;
+            }
+            else
+            {
+                for (int i = Number; i > 0; i--)
+                {
+                    factorialNumber *= i;
+                }
+            }
+
+
+            return factorialNumber;
         }
     }
 }
