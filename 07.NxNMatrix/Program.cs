9a47e175d94b0876ad362bb085e93f99e6f5082d
skider96 <danailqkiq@gmail.com>
2023-10-24 19:21:57 +0300

    solved 7th excercise

--- 07.NxNMatrix/Program.cs
+++ 07.NxNMatrix/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a method that receives a single integer n and prints an NxN matrix using this number as a filler.
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(n);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
