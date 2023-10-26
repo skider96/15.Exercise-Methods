0cddc31c0e425017036110d1d815a323a3209573
skider96 <danailqkiq@gmail.com>
2023-10-26 11:50:11 +0300

    working on

--- 11.ArrayManipulator(not included)/Program.cs
+++ 11.ArrayManipulator(not included)/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> list = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                int index = 0;
+                string[] arguments = command.Split();
+                if (arguments[0] == "exchange")
+                {
+                    index = int.Parse(arguments[1]);
+                    if (index < 0 || index >= list.Count)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    else
+                    {
+                        List<int> partOfList = new List<int>();
+                        partOfList = list.GetRange(index, list.Count - 1);
+                        list.RemoveRange(index, list.Count - 1);
+                        list.InsertRange(0, partOfList);
+                    }
+                }
+                else if (arguments[0] == "max")
+                {
+                    if (arguments[1] == "even")
+                    {
+
+                    }
+                    else if (arguments[1] == "odd")
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
